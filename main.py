e93be3a06e555e30b6cae1b0d3dbb5985e0d0f97
KeigoMega <31650689+KeigoMega@users.noreply.github.com>
2020-12-21 13:04:54 +0900

    v1221-1304 toriaezu finished

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-# v1218-1724
+# v1221-1304
 
 import sys
+import threading
+import modules
 from copy import deepcopy
 
 DEFAULT_IMAGE_PATH = 'C:\\TEMP\\QR.bmp'
@@ -42,6 +44,12 @@ class QR2LINES:
         # Image Array
         self.image_array = []
         self.image_start = 0
+
+        # Drawing Points
+        self.start_point_x = 0
+        self.end_point_x = 0
+        self.start_point_y = 0
+        self.end_point_y = 0
 
     def openImageFile(self, filepath=''):
         if filepath == '':
@@ -110,19 +118,121 @@ class QR2LINES:
 
         for axis_y in range(self.image_height):
             for axis_x in range(loop_x4):
-                borw = int.from_bytes(self.rd_file.read(1), 'little')
-
+                borw = 255 - int.from_bytes(self.rd_file.read(1), 'little')
                 #ここでbit単位に分解だ!!!
-
-                self.image_array[axis_y].append(borw)
+                borw_binlist = reversed(modules.num2binList(borw, 8))
+                borw_binlist = borw_binlist
+                self.image_array[axis_y].extend(borw_binlist)
+            self.image_array[axis_y] = self.image_array[axis_y][: self.image_width]
+        # debug print
         for arr in self.image_array:
-            print(arr)
+            #print(arr)
+            pass
+
+    def searchDrawPoint(self, axis_x, axis_y, xory, start_or_end):
+        temp_x = axis_x
+        draw_x = axis_x
+        temp_y = axis_y
+        draw_y = axis_y
+        ending = 0
+
+        if xory == 'x':
+            if start_or_end == 'start':
+                # search true start_point_x
+                while not ending:
+                    temp_x = draw_x - 1
+                    temp_x = 0 if temp_x < 0 else temp_x
+                    if self.image_array[axis_y][temp_x]:
+                        draw_x = temp_x
+                        if draw_x == 0:
+                            ending = 1
+                    else:
+                        ending = 1
+                self.start_point_x = draw_x * 0.5
+
+            elif start_or_end == 'end':
+                # search true end_point_x
+                while not ending:
+                    temp_x = draw_x + 1
+                    temp_x = self.image_width-1 if temp_x > self.image_width-1 else temp_x
+                    if self.image_array[axis_y][temp_x]:
+                        draw_x = temp_x
+                        if draw_x == self.image_width-1:
+                            ending = 1
+                    else:
+                        ending = 1
+                self.end_point_x = draw_x * 0.5
+        elif xory == 'y':
+            if start_or_end == 'start':
+                # search true start_point_y
+                while not ending:
+                    temp_y = draw_y - 1
+                    temp_y = 0 if temp_y < 0 else temp_y
+                    if self.image_array[temp_y][axis_x]:
+                        draw_y = temp_y
+                        if draw_y == 0:
+                            ending = 1
+                    else:
+                        ending = 1
+                self.start_point_y = draw_y * 0.5
+            elif start_or_end == 'end':
+                # search true end_point_y
+                while not ending:
+                    temp_y = draw_y + 1
+                    temp_y = self.image_height-1 if temp_y > self.image_height-1 else temp_y
+                    if self.image_array[temp_y][axis_x]:
+                        draw_y = temp_y
+                        if draw_y == self.image_height:
+                            ending = 1
+                    else:
+                        ending = 1
+                self.end_point_y = draw_y * 0.5
+
+    def arrayToDrawing(self):
+        drawing_line_set = set()
+        for axis_y in range(self.image_height):
+            for axis_x in range(self.image_width):
+                if self.image_array[axis_y][axis_x]:
+                    # search drawinf points
+                    args1 = (axis_x, axis_y, 'x', 'start')
+                    args2 = (axis_x, axis_y, 'x', 'end')
+                    args3 = (axis_x, axis_y, 'y', 'start')
+                    args4 = (axis_x, axis_y, 'y', 'end')
+                    args_set = [args1, args2, args3, args4]
+                    th_list = []
+                    # never use multiprocessing!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    for args_exe in args_set:
+                        th = threading.Thread(target=self.searchDrawPoint, args=args_exe)
+                        th.start()
+                        th_list.append(th)
+                    # wait for threads finish
+                    for thz in th_list:
+                        thz.join()
+                    # make horizonal drawing
+                    if self.start_point_x - self.end_point_x:
+                        drawing_line_set.add(str(f'{self.start_point_x} {axis_y*0.5}_{self.end_point_x} {axis_y*0.5}'))
+                    # make vertical drawing
+                    if self.start_point_y - self.end_point_y:
+                        drawing_line_set.add(str(f'{axis_x*0.5} {self.start_point_y}_{axis_x*0.5} {self.end_point_y}'))
+
+        # debug print
+        #print(drawing_line_set)
+
+        return drawing_line_set
+
+    def writeOutTextFile(self, filename='', all_text=''):
+        with open(filename, mode='w', encoding='Shift-JIS') as op_file:
+            for lines in all_text:
+                op_file.write(lines[: lines.find('_')]+'\n')
+                op_file.write(lines[lines.find('_')+1: ]+'\n')
 
     def sequenceImageToLines(self, filepath):
         self.getImageHeaders(filepath)
         self.getImageFormat()
         self.checkImageFormat()
         self.imageToArray(filepath)
+        result = self.arrayToDrawing()
+        self.writeOutTextFile('C:\\TEMP\\QR.txt', result)
 
 def main():
     qr_cls = QR2LINES()
